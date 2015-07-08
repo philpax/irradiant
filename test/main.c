7135ec639f8d1dfda830b608187009ae33157d8a
Philpax <me@philpax.me>
2015-07-09 03:20:11 +1000

    Added stb_perlin.h to test cases

--- test/main.c
+++ test/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define STB_PERLIN_IMPLEMENTATION
+#include "stb_perlin.h"
 
 int add(int a, int b)
 {
@@ -16,6 +18,9 @@ int main(int argc, char** argv)
 
     if (argc > 3)
         printf("Sum: %d\n", add(atoi(argv[2]), atoi(argv[3])));
+
+    float x = 3.14159, y = 13.37, z = 42.42;
+    printf("Perlin noise at (%f, %f, %f): %f\n", x, y, z, stb_perlin_noise3(x, y, z, 0, 0, 0));
 
     return 0;
 }

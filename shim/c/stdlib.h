64b06e4ddc6abae7c9ce7a24ab4ea56c9126a630
Philpax <me@philpax.me>
2015-07-08 20:57:02 +1000

    Added atoi to shims, as well as an addition test case

---
+++ shim/c/stdlib.h
@@ -0,0 +1,1 @@
+int atoi(char const*);
--- test/main.c
+++ test/main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
 
 int main(int argc, char** argv)
 {
@@ -7,6 +13,9 @@ int main(int argc, char** argv)
     if (argc > 1)
         printf(" and %s", argv[1]);
     printf("!\n");
+
+    if (argc > 3)
+        printf("Sum: %d\n", add(atoi(argv[2]), atoi(argv[3])));
 
     return 0;
 }

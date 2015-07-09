0dadefe20c807b80e39dedf2e3baffd56a27b01c
Philpax <me@philpax.me>
2015-07-09 14:02:38 +1000

    Added 99 Bottles as a test case

--- shim/c/stdlib.h
+++ shim/c/stdlib.h
@@ -1,1 +1,4 @@
 int atoi(char const*);
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
---
+++ test/99bottles.c
@@ -0,0 +1,16 @@
+// Sourced from http://rosettacode.org/wiki/99_Bottles_of_Beer#The_simple_solution
+#include <stdlib.h>
+#include <stdio.h>
+ 
+int main(void)
+{
+  unsigned int bottles = 99;
+  do
+  {
+    printf("%u bottles of beer on the wall\n", bottles);
+    printf("%u bottles of beer\n", bottles);
+    printf("Take one down, pass it around\n");
+    printf("%u bottles of beer on the wall\n\n", --bottles);
+  } while(bottles > 0);
+  return EXIT_SUCCESS;
+}
